4bc5535f4788db3b44c7d2534b86301f34bd16eb
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-03-08 14:12:35 +0100

    Esercizio terminato

--- src/pages/Prodotti.jsx
+++ src/pages/Prodotti.jsx
@@ -1,6 +1,29 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 export default function Prodotti(){
+    
+    const [prodotti, setProdotti] = useState([])
+    
+    function fetchProdotti(){
+        axios.get("https://fakestoreapi.com/products")
+            .then((r) => {
+                setProdotti(r.data)
+                console.log(r.data)
+    })
+    }
+
+    useEffect(fetchProdotti,[])
+
     return(
         <>
+            <h2 className="text-center">Prodotti</h2>
+            <p className="text-center">Una lista dei nostri prodotti disponibili:</p>
+            <ul>
+                {prodotti.map((prodotto) => (
+                    <li key={prodotto.id}>{prodotto.title}</li>
+                ))}
+            </ul>
         </>
     )
 }

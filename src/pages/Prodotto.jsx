8d6545321a69b6bd024a459de816203fd14f108a
massimilianomiceli <miceli.massimiliano@geoconsultsrl.it>
2026-03-12 18:01:05 +0100

    aggiunto loader durante chiamata ajax

--- src/pages/Prodotto.jsx
+++ src/pages/Prodotto.jsx
@@ -7,9 +7,13 @@ export default function Prodotto(){
 
     const {id}=useParams()
     const navigate = useNavigate()
-    const [prodotto, setProdotto] = useState([])
+    const [prodotto, setProdotto] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
     
     function fetchProdotto(){
+
+        setIsLoading(true) 
+
         axios.get(`https://fakestoreapi.com/products/${id}`)
             .then((r) => {
 
@@ -27,19 +31,29 @@ export default function Prodotto(){
                     return
                 }
             })
+            .finally(() => {
+                setIsLoading(false)                
+            }
+            )
     }
 
     useEffect(fetchProdotto,[id])
 
     return(
         <>
-        <div className="card">
-            <img className="card-img-top" src={prodotto.img} alt="Card image cap"/>
-            <div className="card-body">
-                <h1>{prodotto.title}</h1>
-                <p className="card-text">{prodotto.description}</p>
+        {
+            isLoading ? (
+                <img className="" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif" alt="Loader"/>
+            ) : (
+                <div className="card">
+                <img className="card-img-top" src={prodotto.image} alt="Card image cap"/>
+                <div className="card-body">
+                    <h1>{prodotto.title}</h1>
+                    <p className="card-text">{prodotto.description}</p>
+                </div>
             </div>
-        </div>
+            )
+        }
         <button onClick={() => navigate(`/Prodotti/${prodotto.id - 1}`)}>Indietro</button>
         <button onClick={() => navigate(`/Prodotti/${prodotto.id + 1}`)}>Avanti</button>
         </>
